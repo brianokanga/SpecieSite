5c72fe20759634bac40f2db5ddfda1ae2526ff86
Brian Okanga <briannokanga@gmail.com>
2020-10-02 13:03:28 +0300

    Added Plant request model

---
+++ Species.Data/Models/PlantRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Species.Data.Models
+{
+    public class PlantRequest
+    {
+        public int Id { get; set; }
+
+        public int CountyId { get; set; }
+        public int SubCountyId { get; set; }
+        public int SpecieId { get; set; }
+        public int SpecieInformationId { get; set; }
+        public County County { get; set; }
+        public SubCounty SubCounty { get; set; }
+        public Location Location { get; set; }
+        public Specie Specie { get; set; }
+        public SpecieInformation SpecieInformation { get; set; }
+
+    }
+}
